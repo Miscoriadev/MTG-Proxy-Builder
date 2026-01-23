5ef2de7979fb74f0b488b049f8a033f51016a3b9
Praevalens <10863613+Praevalens@users.noreply.github.com>
2026-01-23 23:20:33 +0100

    fix: update deployment branch to production and adjust YAML formatting

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/mtg-proxy-builder/',
+  base: '/MTG-Proxy-Builder/',
   plugins: [react()],
   resolve: {
     alias: {

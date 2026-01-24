ea150cf8b5347708a658d4d6818e755af710b5fa
Praevalens <10863613+Praevalens@users.noreply.github.com>
2026-01-24 14:11:32 +0100

    fix: remove base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/MTG-Proxy-Builder/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
